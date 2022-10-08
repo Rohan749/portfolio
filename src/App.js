eb0f15116c2540d2ac315ba4828d2eba5c584599
Rohan Kumar Pandey <rohanpandey749@gmail.com>
2022-10-08 08:28:57 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React from "react";
 import FrontPage from "./Components/FrontPage/FrontPage";
 import ProjectSection from "./Components/ProjectSection/ProjectSection";
 import Skills from "./Components/Skills&Edu/Skills";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import {BrowserRouter as Router, Routes, Route, HashRouter} from "react-router-dom"
 
 function App() {
   return (
     <>
       <Router>
+      <HashRouter basename="/rohan-portfolio-website/">
         <Routes>
           <Route path="/rohan-portfolio-website/" element={<FrontPage />} />
           <Route path="/rohan-portfolio-website/projects" element={<ProjectSection />} />
           <Route path="/rohan-portfolio-website/skills" element={<Skills />} />
         </Routes>
+      </HashRouter>
       </Router>
     </>
   );
