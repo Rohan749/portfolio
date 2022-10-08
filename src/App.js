49ff1511abc2a6181f475fc068e80cb5f811b289
Rohan Kumar Pandey <rohanpandey749@gmail.com>
2022-10-08 08:42:00 +0530

    added switch statement

--- src/App.js
+++ src/App.js
@@ -2,19 +2,33 @@ import React from "react";
 import FrontPage from "./Components/FrontPage/FrontPage";
 import ProjectSection from "./Components/ProjectSection/ProjectSection";
 import Skills from "./Components/Skills&Edu/Skills";
-import {BrowserRouter as Router, Routes, Route, HashRouter} from "react-router-dom"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  HashRouter,
+} from "react-router-dom";
+import { Switch } from "@mui/material";
 
 function App() {
   return (
     <>
       <Router>
-      <HashRouter basename="/rohan-portfolio-website/">
-        <Routes>
-          <Route path="/rohan-portfolio-website/" element={<FrontPage />} />
-          <Route path="/rohan-portfolio-website/projects" element={<ProjectSection />} />
-          <Route path="/rohan-portfolio-website/skills" element={<Skills />} />
-        </Routes>
-      </HashRouter>
+        <HashRouter basename="/rohan-portfolio-website/">
+          <Switch>
+            <Routes>
+              <Route path="/rohan-portfolio-website/" element={<FrontPage />} />
+              <Route
+                path="/rohan-portfolio-website/projects"
+                element={<ProjectSection />}
+              />
+              <Route
+                path="/rohan-portfolio-website/skills"
+                element={<Skills />}
+              />
+            </Routes>
+          </Switch>
+        </HashRouter>
       </Router>
     </>
   );
