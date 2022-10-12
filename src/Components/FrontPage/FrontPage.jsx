6421a583086d8823a6070e45c8e4d9327ac61ae6
Rohan Kumar Pandey <rohanpandey749@gmail.com>
2022-10-12 18:51:37 +0530

    initial changes

--- src/Components/FrontPage/FrontPage.jsx
+++ src/Components/FrontPage/FrontPage.jsx
@@ -12,40 +12,50 @@ const FrontPage = () => {
   return (
     <>
       <div className="leftside" />
-      
+
       <div id="home" className="background">
-        <motion.div className="profile"
-        initial={{ y: "15vh", opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 1, delay: 1.5 }}
+        <motion.div
+          className="profile"
+          initial={{ y: "15vh", opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1, delay: 1.5 }}
         ></motion.div>
-        <motion.div className="name"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.8, delay: 2.9 }}
-        >ROHAN</motion.div>
-        <motion.div className="surname"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.8, delay: 3.7 }}
-        >KUMAR PANDEY</motion.div>
-        <motion.div className="desc"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.8, delay: 3.7}}
+        <motion.div
+          className="name"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.8, delay: 2.9 }}
         >
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti
-          molestiae voluptates ipsa quo repudiandae iusto rem incidunt error
-          consectetur itaque. Quasi dolore tempore aspernatur debitis
-          praesentium magnam facere. Praesentium, debitis.
+          ROHAN
         </motion.div>
-        <motion.div className="icons"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.8, delay: 4.5}}
+        <motion.div
+          className="surname"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.8, delay: 3.7 }}
         >
-        <nav>
-            <a className="a_link"
+          KUMAR PANDEY
+        </motion.div>
+        <motion.div
+          className="desc"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.8, delay: 3.7 }}
+        >
+          Hello, my name is Rohan. I'm a front-end developer and content
+          creator. I believe I'm an enthusiastic person who loves to learn new
+          skills and gain experience. I regularly create content based on
+          Development, React, JavaScript, and many more! Connect with me.
+        </motion.div>
+        <motion.div
+          className="icons"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.8, delay: 4.5 }}
+        >
+          <nav>
+            <a
+              className="a_link"
               href="https://www.linkedin.com/in/rohan-kumar-pandey-25a263208/"
               target="_blank"
             >
@@ -53,22 +63,35 @@ const FrontPage = () => {
             </a>
           </nav>
           <nav>
-            <a className="a_link" href="https://www.instagram.com/_r.code_/" target="_blank">
+            <a
+              className="a_link"
+              href="https://www.instagram.com/_r.code_/"
+              target="_blank"
+            >
               <BsInstagram />
             </a>
           </nav>
           <nav>
-            <a className="a_link" href="https://twitter.com/heyrohan7" target="_blank">
+            <a
+              className="a_link"
+              href="https://twitter.com/heyrohan7"
+              target="_blank"
+            >
               <GrTwitter />
             </a>
           </nav>
           <nav>
-            <a className="a_link" href="https://github.com/Rohan749" target="_blank">
+            <a
+              className="a_link"
+              href="https://github.com/Rohan749"
+              target="_blank"
+            >
               <BsGithub />
             </a>
           </nav>
           <nav>
-            <a className="a_link"
+            <a
+              className="a_link"
               href="https://auth.geeksforgeeks.org/user/rohanpandey749/practice"
               target="_blank"
             >
@@ -76,18 +99,31 @@ const FrontPage = () => {
             </a>
           </nav>
         </motion.div>
-        <motion.div className="personal_info"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.8, delay: 4.5 }}
+        <motion.div
+          className="personal_info"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.8, delay: 4.5 }}
         >
-          <div className="info email"><strong>Email:</strong> <br /> rohanpandey749@gmail.com</div>
-          <div className="info phno"><strong>Ph No.:</strong> <br /> 8210036627</div>
-          <a href="https://drive.google.com/file/d/1xyQQK6hRdaWSjonEpjcXOnpXt52jbMhy/view?usp=sharing"
-            target="_blank"><button className="info resume">Download Resume<HiClipboardCopy className="clip"/></button></a>
+          <div className="info email">
+            <strong>Email:</strong> <br /> rohanpandey749@gmail.com
+          </div>
+          <div className="info phno">
+            <strong>Ph No.:</strong> <br /> 8210036627
+          </div>
+          <a
+            href="https://drive.google.com/file/d/1xyQQK6hRdaWSjonEpjcXOnpXt52jbMhy/view?usp=sharing"
+            target="_blank"
+          >
+            <button className="info resume">
+              Download Resume
+              <HiClipboardCopy className="clip" />
+            </button>
+          </a>
           <br />
-          <a href="https://www.showwcase.com/rohan749"
-            target="_blank"><button className="info showcase">Showwcase</button></a>
+          <a href="https://www.showwcase.com/rohan749" target="_blank">
+            <button className="info showcase">Showwcase</button>
+          </a>
         </motion.div>
       </div>
     </>
